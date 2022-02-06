ffc488e6e8ef17c7a2389c0b7252c8927194ae6e
Fagner Viana <fagner.viana@aluno.ifsp.edu.br>
2022-02-06 19:47:15 -0300

    Iniciando projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import Filter from './components/filter/index-filter-interno';
+import Map from './components/map/index-map-interno';
+function App() {
+  return (
+    <div className="App">
+      <Map/>
+    </div>
+  );
+}
+
+export default App;
